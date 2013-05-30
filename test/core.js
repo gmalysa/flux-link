21fb364e543d6730769211c333d09d7514349040
Greg Malysa <gmalysa@stanford.edu>
2013-05-30 22:01:40 +0000

    Added tests for loop and parallel chains

--- test/core.js
+++ test/core.js
@@ -5,26 +5,32 @@ exports = {};
 exports['simple'] = function(test) {
 	var chain = new fl.Chain(
 		function(env, after) {
+			test.ok(true);
 			after();
 		});
 	var env = new fl.Environment();
+
+	test.expect(1);
 	chain.call(null, env, test.done);
 }
 
 exports['nested chains'] = function(test) {
 	var chain = new fl.Chain(
 		function(env, after) {
+			test.ok(true);
 			after();
 		});
 	var outer = new fl.Chain(chain, chain);
-
 	var env = new fl.Environment();
+
+	test.expect(2);
 	outer.call(null, env, test.done);
 }
 
 exports['explicit parameters'] = function(test) {
 	var chain = new fl.Chain(
 		function(env, after) {
+			test.ok(true);
 			after(1);
 		}, function(env, after, param) {
 			test.equal(param, 1);
@@ -32,6 +38,7 @@ exports['explicit parameters'] = function(test) {
 		});
 
 	var env = new fl.Environment();
+	test.expect(2);
 	chain.call(null, env, test.done);
 }
 
@@ -39,6 +46,7 @@ exports['stack parameters'] = function(test) {
 	var chain = new fl.Chain(
 		function(env, after) {
 			env.$push(1);
+			test.ok(true);
 			after();
 		},
 		function(env, after, param) {
@@ -47,12 +55,14 @@ exports['stack parameters'] = function(test) {
 		});
 
 	var env = new fl.Environment();
+	test.expect(2);
 	chain.call(null, env, test.done);
 }
 
 exports['chained explicit parameters'] = function(test) {
 	var chain = new fl.Chain(
 		function(env, after) {
+			test.ok(true);
 			after(1, 2);
 		},
 		function(env, after, param) {
@@ -65,6 +75,7 @@ exports['chained explicit parameters'] = function(test) {
 		});
 	
 	var env = new fl.Environment();
+	test.expect(3);
 	chain.call(null, env, test.done);
 }
 
@@ -73,6 +84,7 @@ exports['chained stack parameters'] = function(test) {
 		function(env, after) {
 			env.$push(1);
 			env.$push(2);
+			test.ok(true);
 			after();
 		},
 		function(env, after, param) {
@@ -85,6 +97,45 @@ exports['chained stack parameters'] = function(test) {
 		});
 	
 	var env = new fl.Environment();
+	test.expect(3);
+	chain.call(null, env, test.done);
+}
+
+exports['loop'] = function(test) {
+	var chain = new fl.LoopChain(
+		function(env, after) {
+			test.ok(true);
+			if (env.count > 0)
+				after(true);
+			else
+				after(false);
+		},
+		function(env, after) {
+			env.count -= 1;
+			after();
+		});
+	
+	var env = new fl.Environment({count : 3});
+	test.expect(4);	// 4 checks, 3 iterations of the body
+	chain.call(null, env, test.done);
+}
+
+exports['parallel'] = function(test) {
+	var dec = function dec(env, after) {
+		env._env.count -= 1;
+		test.ok(true);
+		after();
+	};
+
+	var pc = new fl.ParallelChain(dec, dec, dec, dec, dec);
+	var chain = new fl.Chain(pc,
+		function(env, after) {
+			test.equals(env.count, 0);
+			after();
+		});
+	
+	var env = new fl.Environment({count : 5});
+	test.expect(6);
 	chain.call(null, env, test.done);
 }
 

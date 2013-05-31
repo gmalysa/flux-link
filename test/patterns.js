d8f1c31dc93dfaa5e8beb65dfb31f87562f2052e
Greg Malysa <gmalysa@stanford.edu>
2013-05-31 18:07:09 +0000

    Added map pattern + test case

---
+++ test/patterns.js
@@ -0,0 +1,24 @@
+var fl = require('../lib-cov/flux-link');
+var p = require('../lib-cov/patterns');
+
+exports = {};
+
+exports['map'] = function(test) {
+	var input = [1, 2, 3];
+	var result = [1, 4, 9];
+	var sq = function(env, after, v, k, i, list) {
+		after(v*v);
+	};
+	
+	var chain = new fl.Chain(p.map(sq),
+		function(env, after, map_result) {
+			test.deepEqual(result, map_result);
+			after();
+		});
+
+	var env = new fl.Environment();
+	test.expect(1);
+	chain.call(null, env, test.done, input);
+};
+
+module.exports = exports;

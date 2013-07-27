e728bb40e1cef07cc607323844d46bcb44305e3f
Greg Malysa <gmalysa@stanford.edu>
2013-07-27 17:52:40 +0000

    Added serial filter pattern

--- test/patterns.js
+++ test/patterns.js
@@ -57,6 +57,24 @@ exports['filter'] = function(test) {
 	chain.call(null, env, test.done, input);
 };
 
+exports['sfilter'] = function(test) {
+	var input = [1, 2, 3, 4];
+	var result = [2, 4];
+	var filt = function(env, after, v, k, list) {
+		after(v % 2 == 0);
+	};
+
+	var chain = new fl.Chain(fl.p.sfilter(filt),
+		function(env, after, filter_result) {
+			test.equal(_.difference(result, filter_result).length, 0);
+			after();
+		});
+
+	var env = new fl.Environment();
+	test.expect(1);
+	chain.call(null, env, test.done, input);
+};
+
 exports['each'] = function(test) {
 	var input = [1, 2, 3, 4];
 	var each = function(env, after, v, k, list) {

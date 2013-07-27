ba891f040fced4ed2602b279701eb7604c99c1ba
Greg Malysa <gmalysa@stanford.edu>
2013-07-27 17:23:54 +0000

    Added serial map pattern.

--- test/patterns.js
+++ test/patterns.js
@@ -21,6 +21,24 @@ exports['map'] = function(test) {
 	chain.call(null, env, test.done, input);
 };
 
+exports['smap'] = function(test) {
+	var input = [1, 2, 3];
+	var result = [1, 4, 9];
+	var sq = function(env, after, v, k, i, list) {
+		after(v*v);
+	};
+	
+	var chain = new fl.Chain(fl.p.smap(sq),
+		function(env, after, map_result) {
+			test.deepEqual(result, map_result);
+			after();
+		});
+
+	var env = new fl.Environment();
+	test.expect(1);
+	chain.call(null, env, test.done, input);
+}
+
 exports['filter'] = function(test) {
 	var input = [1, 2, 3, 4];
 	var result = [2, 4];

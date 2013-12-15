f51f5abbad308612b6b330275376e154f440fc07
Greg Malysa <gmalysa@stanford.edu>
2013-12-15 08:00:18 +0000

    Fix for #1 plus test cases

--- test/exceptions.js
+++ test/exceptions.js
@@ -15,4 +15,41 @@ exports['env.$catch()'] = function(test) {
 	chain.call(null, env, test.done);
 }
 
+exports['exception chaining if unhandled'] = function(test) {
+	var chain = new fl.Chain(
+		function(env, after) {
+			env.$throw(new Error('exception!'));
+		});
+	var oc = new fl.Chain(chain);
+	oc.set_exception_handler(function(env, err) {
+		env.$catch();
+	});
+
+	var env = new fl.Environment();
+	oc.call(null, env, function() { test.ok(true); test.done(); });
+}
+
+exports['bind_after_env applies in exception'] = function(test) {
+	var chain = new fl.Chain(
+		function(env, after) {
+			env.testval = 1;
+			after();
+		},
+		function(env, after) {
+			env.$throw(new Error('throw error'));
+		});
+	chain.set_bind_after_env(true);
+
+	chain.set_exception_handler(function(env, arr) {
+		env.$catch();
+	});
+
+	var env = new fl.Environment();
+	env.testval = 0;
+	chain.call(null, env, function(env) {
+		test.equals(env.testval, 1);
+		test.done();
+	});
+}
+
 module.exports = exports;
